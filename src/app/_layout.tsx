0e189b64aa9371202525a91168b6b4b0cf7b49c4
Yrsa Cueto Zarate <suhamyzarate@gmail.com>
2026-08-05 19:54:52 -0500

    Protección de acceso por sesión

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,45 @@
 import { Stack } from "expo-router";
+import {
+  onAuthStateChanged,
+  User,
+} from "firebase/auth";
+import { useEffect, useState } from "react";
 
+import { firebaseAuth } from "@/config/firebase/firebase.config";
 import { ThemeProvider } from "@/core/contexts/theme.context";
 
 export default function RootLayout() {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(
+      firebaseAuth,
+      (currentUser) => {
+        setUser(currentUser);
+        setIsLoading(false);
+      },
+    );
+
+    return unsubscribe;
+  }, []);
+
+  if (isLoading) {
+    return null;
+  }
+
   return (
     <ThemeProvider>
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" />
-        <Stack.Screen name="(auth)" />
-        <Stack.Screen name="(tabs)" />
+
+        <Stack.Protected guard={user === null}>
+          <Stack.Screen name="(auth)" />
+        </Stack.Protected>
+
+        <Stack.Protected guard={user !== null}>
+          <Stack.Screen name="(tabs)" />
+        </Stack.Protected>
       </Stack>
     </ThemeProvider>
   );
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,13 @@
 import { Redirect } from "expo-router";
 
+import { firebaseAuth } from "@/config/firebase/firebase.config";
+
 export default function Index() {
-  return <Redirect href="/login" />;
+  const user = firebaseAuth.currentUser;
+
+  return (
+    <Redirect
+      href={user ? "/products" : "/login"}
+    />
+  );
 }
