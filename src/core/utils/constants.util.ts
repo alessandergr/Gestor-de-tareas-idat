6bfdb281dc1ceebf88a431d2779f2868f87e57fd
Salvinia Palomino Ochoa <salviniaed@gmail.com>
2026-08-06 21:14:55 -0500

    Limpiar archivos y dependencias innecesarias

--- src/core/utils/constants.util.ts
+++ src/core/utils/constants.util.ts
@@ -3,18 +3,3 @@ export const ICON_SIZES = {
   md: 24,
   lg: 48,
 };
-
-export const ICON_CONTAINER_SIZES = {
-  sm: {
-    width: 24,
-    height: 24,
-  },
-  md: {
-    width: 48,
-    height: 48,
-  },
-  lg: {
-    width: 72,
-    height: 72,
-  },
-};
